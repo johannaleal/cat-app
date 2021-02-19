b76ab3a52d8516ed77745dcc23e372d088f81b52
Johanna L <johannarleal@gmail.com>
2021-02-19 13:40:18 -0500

    Setup connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: '',
-  database: 'cat_db',
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  // connect to remote db
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      // NOTE: Be sure to add your MySQL password here!
+      password: 'Trilogy2021',
+      database: 'cat_db',
+    });
+};
 
 // Make connection.
 connection.connect((err) => {
